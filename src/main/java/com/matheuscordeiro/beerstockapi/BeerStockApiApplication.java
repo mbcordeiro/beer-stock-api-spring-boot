b5b22cda054e575153875db35ea474cc2ce8da0c
mbcordeiro <matheusdebarroscorderio@gmail.com>
2020-12-08 00:56:41 -0300

    First commit

---
+++ src/main/java/com/matheuscordeiro/beerstockapi/BeerStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.beerstockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerStockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscordeiro/beerstockapi/BeerStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.beerstockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
